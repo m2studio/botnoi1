7dafaf44ee69b70be505fd2de0dc9753bc85b83a
Jirapat Kittithanakunanan <jirapat.kittithanakunanan@agoda.com>
2021-09-05 00:52:55 +0700

    update tensorflow to 2.4.3

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 import os
 from flask import Flask, flash, request, render_template
 from datetime import datetime
-#import pickle
-#import botnoi as bn
-#from botnoi import cv
+import pickle
+import botnoi as bn
+from botnoi import cv
 
 UPLOAD_DIRECTORY = './images/'
 app = Flask(__name__)
 
-# model_file = 'cloud_predict.pickle'
-# model = pickle.load(open(model_file,'rb'))
+model_file = 'cloud_predict.pickle'
+model = pickle.load(open(model_file,'rb'))
 
 @app.route('/')
 def hello_world():
@@ -49,11 +49,11 @@ def get_image_path(cloud_type):
     }
     return switcher.get(cloud_type, '<img src="/static/clouds/altocumulus.png" width="400px" />')
 
-# def test(filename):
-#     a = cv.image(filename)
-#     feat = a.getmobilenet()
-#     res = model.predict([feat])
-#     return str(res[0])
+def test(filename):
+    a = cv.image(filename)
+    feat = a.getmobilenet()
+    res = model.predict([feat])
+    return str(res[0])
 
 def read_predict_html():
     with open('prediction.html') as f:
@@ -61,8 +61,8 @@ def read_predict_html():
 
 def get_predict_html(filename):
     print(filename)
-    # cloud_type = test(filename)    
-    cloud_type = 'nimbostratus' #fix the cloud type TODO: call model.predict with the given filename to get cloud_type
+    cloud_type = test(filename)    
+    # cloud_type = 'nimbostratus' #fix the cloud type TODO: call model.predict with the given filename to get cloud_type
     description = get_description(cloud_type)
     predict_html = read_predict_html()
     predict_html = predict_html.replace('__PREDICTION__', cloud_type + ' : ' + description)
